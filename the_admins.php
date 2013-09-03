7caef1e33e12bb1c2b2fab417858aa5f6d0ff44c
Perry Shuman <fortytwofortytwo@gmail.com>
2013-09-03 16:08:17 -0400

    Meating the new fall admins.

--- the_admins.php
+++ the_admins.php
@@ -30,107 +30,71 @@
 	
    <div id="content" class="">
 	       
-		<h2 style="display:inline; float:left;line-height:18px;">Meet the Admins</h2>
+		<h2 style="display:inline; float:left;line-height:18px;">Meat the Admins</h2>
 		<h3 style="display:inline; float:right; line-height:18px;"><a href="contact.php">Email the Admins</a></h3>
 		<p style="clear:both; line-height:0px;">&nbsp;</p>
 		
 		<div class="mta0">
-			<img src="images/admins/scuba.jpg">
-			<h3>Scuba</h3>
-			<h4 id="scuba"> Intern - Mission Critiquer - General Angry Scuba</h4>
+			<img src="images/admins/PIMP_ADMIN.jpg">
+			<h3>Kersh</h3>
+			<h4 id="kersh"> Head Admin - Runner - Master of All Truths - Pimp Admin</h4>
 			<p>
-				As the oldest person on the team this makes me the most wise, right? No, darn. So I have been Adminning for a few games now, so I know how this whole deal works. I help with some of the mission writing for both sides. I also have become the role player for some of the missions. I am here to help the zombies win... wait that's not right. I am here to help the game run as smoothly as possible which can cause some loud noises to emanate from my direction at points.
+				Why hello everyone! Kersh is the name. I am here to make sure that everyone has a fun and exciting week of resisting, nomming, and overall fighting the good fight. This will be my fourth time adminning, so I do know a thing or two about the game. Feel free to approach me in any wide open and easily accessible area that is in play for nice long conversations about how to be a successful human player. Zombies are also welcome to BRAINS BRAINS BRAINS! And remember kids,
+				<strong>DON'T BE A JERK!</strong>
 			</p>
 		</div>
 
        <div class="mta1">
-			<img src="images/admins/brad.jpg">
-			<h3>Brad VanSlyke</h3>
-            <h4 id="brad"> Glorious Head Admin - Defender of the Righteous - Slayer of the Wicked</h4>
+			<img src="images/admins/wes.jpg">
+			<h3>Wes</h3>
+            <h4 id="wes"> Co-Head Admin, God of Thunder Admin</h4>
 			<p>
-				This is my third game as an Admin, and my final game as a Head Admin.  I've been involved in every game except for the very first.  Leader of the Resistance, General of the Horde, Original Zombie, Hivemind, Survivor till the Finale, Previous Record Holder for Circle of Death, and as an all around take charge player I've done a lot within HvZ.  Now as a hopefully graduating 5th Year CS Major, it's my job to make sure everyone has a complete blast in this semester's game!
+				Previously, four games, once as OZ. Now, known as Thor, third game as admin, and first game as co-head admin. If you&#x2019;re not having fun, if you&#x2019;re not being smart, if you&#x2019;re trying to break the game, you&#x2019;re doing it wrong. Now go forth, stun and feed until your heart's content!
 			</p>
 		</div>
        
        <div class="mta0">
 			<img src="images/admins/kake.jpg">
 			<h3>Kate Hoyt</h3>
-			<h4 id="kake"> (ex)Head Admin - Mission Critique-er - Plot Writer</h4>
+			<h4 id="kake"> Past-Head Admin, Mission Critique-er, &amp; Overall Oldbie</h4>
 			<p>
-                Hallo! I used to be one of the ones that made the tough decisions, but now I've returned as a regular admin for my fifth game of HvZ. I do mission and plot writing, and during planning I remind the other admins when their ideas are too complicated. I help coordinate missions and you'll likely see me a lot. Remember to listen when your friendly admin-bot projects loud noises, it tends to be important!	
+                Hallo! I used to be one of the ones that made the tough decisions, but now I&#x2019;ve returned as a regular admin for my sixth game of HvZ (10th game woot woot!). I do mission and plot writing, and during planning I remind the other admins when their ideas are too complicated. I help coordinate missions and you&#x2019;ll likely see me a lot. Remember to listen when your friendly admin-bot projects loud noises, it tends to be important!	
            </p>
 		</div>
-       
-		<div class="mta1">
-			<img src="images/admins/PIMP_ADMIN.jpg">
-			<h3>Kersh</h3>
-            <h4 id="kersh"> Runner - Master of All Truths - Pimp Admin</h4>
-			<p>
-				Why hello everyone! Kersh is the name. I am here to make sure that everyone has a fun and exciting week of resisting, nomming, and overall fighting the good fight. This will be my third time adminning, so I do know a thing or two about the game. Feel free to approach me in any wide open and easily accessible area that is in play for nice long conversations about how to be a successful human player. Zombies are also welcome to BRAINS BRAINS BRAINS! And remember kids, <h1><center>DON'T BE A JERK!</center></h1>
-			</p>
-		</div>
 		
-		<div class="mta0">
+		<div class="mta1">
 			<img src="images/admins/perry.jpg">
 			<h3>Perry Shuman</h3>
 			<h4 id="perry"> Benevolent Web Overlord - Dr. jQuery - Fedora Zombie Founder</h4>
 			<p>
 				Twice a zombie and loved every second. If you ever need an admin to camp Skiles with you, feel free to call. Remember, stuns last fifteen minutes, the kills last forever. If the website explodes tell me.
 			</p>
-		</div>		
-		
-		<div class="mta1">
-			<img src="images/admins/allen.jpg">
-			<h3>Allen Rawl</h3>
-			<h4 id="allen"> Keeper of the Peace - Master Yeller - Big Intimidating Admin</h4>
-			<p>
-				Hello everyone! This is my third game as an admin, but I have played two fall games and spectated last spring. I have experience in dealing with large crowds, so I am looking forward to making sure nothing stupid happens because, trust me, it probably will.
-			</p>
 		</div>
-
+		
 		<div class="mta0">
-			<img src="images/admins/wes.jpg">
-			<h3>Wes Hunt</h3>
-			<h4 id="wes"> God of Thunder Admin</h4>
-			<p>
-				Hey there, nice to meat you! The name's Wes, but you might have heard of me as Thor, God of Thunder. I've played four games, twice surviving to the finish and once as your friendly neighborhood OZ. Please, for the love of the gods, don't do anything stupid/unsafe because I haven't lost my patience in seven years, and I don't think you want to anger the mighty Thor.
-			</p>
-			
-		</div>
-		
-		<div class="mta1">
 			<img src="images/admins/james.jpg">
 			<h3>James Ray</h3>
-			<h4 id="ray"> God of Mischief Admin</h4>
+			<h4 id="ray"><span style="color: white">Invisible Admin,</span> God of Mischief Admin</h4>
 			<p>
-                Hola gente! James Ray, head lunatic of the group!  I've played this game 4 times now and this is my second time being an admin! Or you may never see me which tends to be the trend, for I am like a shadow, no one worries about him. Well, know that I will be watching. Let the crazy begin!			</p>
+                Hola gente! James Ray, head lunatic of the group!  I&#x2019;ve played this game 4 times now and this is my third time being an admin! Or you may never see me which tends to be the trend, for I am like a shadow, no one worries about him. Well, know that I will be watching. Let the crazy begin!
+            </p>
 		</div>
 
-       <div class="mta0">
+       <div class="mta1">
            <img src="images/admins/kinsey.jpg">
            <h3>Kinsey Canova</h3>
            <h4 id="kinsey"> "Wandering but not lost" Admin</h4>
            <p>
-               Howdy!  I have played one game previously, spending a reasonable amount of time on both sides.  This is my first time as an admin, so I look forward to helping things and whatever else run smoothly in addition to watching the events of game week play out.
-           </p>
-
-       </div>
-
-       <div class="mta1">
-           <img src="images/admins/dewey.jpg">
-           <h3>Dewey Holland</h3>
-           <h4 id="dewey"> Waterjet Master - Ex-Runner Zombie</h4>
-           <p>
-               Peace and Long Life. I am Dewey Holland. A 4th year Mechanical Engineer who has only played two games. However, I was the crazy guy who purposefully went zombie. The reason being to put on a good show and make the game more interesting. This is my first time as an admin. However, I still want to put on a good show. So don't fear the stun timer, don't be a jerk, and have some fun.
+               Howdy!  I have played and adminned one  game each and have enjoyed each thoroughly.  I try to keep the details of missions clear so people know what&#x2019;s going on, and generally think an informed zombie is a dangerous zombie, and an informed human has tastier (but maybe more elusive) brains. 
            </p>
        </div>
 
        <div class="mta0">
            <img src="images/admins/joe.jpg">
            <h3>Joe Russell</h3>
-           <h4 id="joe"> The Thousand-Titled Man - The Alpha - The Second - ...</h4>
+           <h4 id="joe"> The Walking Apocalypse</h4>
            <p>
-               Elen s&iacute;la lumenn' omentielvo. I am Joe the Lightning Fist. Long has my family stood guard against the zombie hordes in the cold northlands. I have participated in HvZ twice and have now ascended to the realms of the Admins. I know the rules forwards and backwards and the gaps in between. I believe that if you die a hero, you rise again to see yourself become a zombie. See you in the field!
+               Elen s&iacute;la lumenn&#x2019; omentielvo. I am Joe the Lightning Fist. Long has my family stood guard against the zombie hordes in the cold northlands. I have participated in HvZ twice and this will be my second game after ascending to the realms of the Admins. I know the rules forwards and backwards and the gaps in between. Just give me a good ship and a star to fly her to. I believe that if you die a hero, you rise again to see yourself become a zombie. See you in the field!
            </p>
 
        </div>
@@ -138,13 +102,58 @@
        <div class="mta1">
            <img src="images/admins/colin.jpg">
            <h3>Colin Sanders</h3>
-           <h4 id="colin"> The Colonel - Obsessive Email Checker</h4>
+           <h4 id="colin"> The Colonel - The Doctor</h4>
            <p>
-               If you ask me for chicken, I will stun you. If you are a human, I will follow you to Woodruff to get some. I've played one game, and this is my first time as an admin, but don't try and tell me sock ricochets don't count, and don't be around me if you aren't having fun. That is all. 
+               Hello everyone! I&#x2019;m a second-time admin, one-time player. I like to resolve disputes, answer e-mails, and bring epic music to epic missions. In this game, F is not for fire that burns down the whole town, so go to missions and have fun or I will smite you. May your life be spent bravely, and may your death be glorious!
+           </p>
+       </div>
+
+       <div class="mta0">
+           <img src="images/admins/albert.jpg">
+           <h3>Albert</h3>
+           <h4 id="albert"> The Evil Twin</h4>
+           <p>
+               Greetings!  I have played the game twice before, and am now a first-semester admin.   I hope to enjoy making this game awesome for everybody.  Be warned:  I do have an identical twin brother on campus.  Tag and zombify him, not me.  Happy hunting! 
+           </p>
+       </div>
+
+       <div class="mta1">
+           <img src="images/admins/daniel.jpg">
+           <h3>Daniel</h3>
+           <h4 id="daniel"> Nerdfighter Admin</h4>
+           <p>
+               Hey guys!  I&#x2019;ve played 4 games, 2 as a human and 2 as a zombie (once OZ!).  This is my first time as an admin and I look forward to helping at all the missions I can.  Be safe, go to missions, and remember, Don&#x2019;t Forget to be Awesome!
+           </p>
+       </div>
+
+       <div class="mta0">
+           <img src="images/admins/somi.jpg">
+           <h3>So-mi Yoon</h3>
+           <h4 id="somi"> Newblet Admin(let)</h4>
+           <p>
+               Good mornin&#x2019;, good afternoon, and good evening! The name&#x2019;s So-mi and I&#x2019;ll be joining the Admin team this year. I&#x2019;ve played HvZ twice, going into the finale as a human once and a zombie the next. Though I may be new to admin-ing, I certainly am not new at gently reprimanding rule-breakers, trolls, and unpleasant people. If you break a rule, I will break you (... Maybe... Most likely). At any rate, have fun and play smart (camping out at Klaus until 11PM is not having fun OR playing smart ;) )!
+           </p>
+       </div>
+
+       <div class="mta1">
+           <img src="images/admins/josh.jpg">
+           <h3>Josh Holley</h3>
+           <h4 id="josh"> Not Sarcastic At All Admin</h4>
+           <p>
+               General rule of thumb: If I am speaking, I am probably being sarcastic. Take that as you will. Hello! I have been part of the game since I was talked into joining freshman year by a friend, so, after 4 games, I decided to try my hand at admining. Zombies shall always prevail, so do not expect me to give you humans the time of day. Get your brain devoured, then you can speak to me. Remember, surviving as a human is only half the game, so do not be lame and rage quit when you inevitably become a zombie. Exact revenge on the humans left and enjoy the game.
+           </p>
+       </div>
+
+       <div class="mta0">
+           <img src="images/admins/tucker.jpg">
+           <h3>Tucker Elliott</h3>
+           <h4 id="tucker"> M. Night Shamalamadingdong</h4>
+           <p>
+               Greetings players! I&#x2019;m a first-time admin who's played the game 4 times. You probably remember me as the guy who played as a war correspondent filming the apocalypse last game (on a related note, I swear I&#x2019;ll release that footage eventually). I&#x2019;m primarily in charge of the plot this year, and we&#x2019;re trying something new in cooperation with BuzzStudios, so I&#x2019;m the guy to (thank/swear vigorously at) if it (works out/fails miserably). Oh, and the safe word is 'banana'. Have fun!
            </p>
        </div>
        
-		<div class="mta0">
+		<div class="mta1">
 			<img src="images/admins/vince.jpg">
 			<h3>Vince Pedicino</h3>
 			<h4 id = "vince"> HvZ's Favorite Mad Professor</h4>
