3f24d1b755c9f355a54ecbd8bfb20816f99b184c
Perry Shuman <fortytwofortytwo@gmail.com>
2013-09-18 11:38:52 -0400

    Re-admins all admins.

---
+++ revealozs.php
@@ -0,0 +1,4 @@
+<?php
+require("scripts/lib.php");
+$db->query("UPDATE users SET faction='ADMIN' WHERE gt_name = 'pshuman3' OR gt_name = 'kcanova3' OR gt_name = 'telliott8' OR gt_name = 'sd301' OR gt_name = 'jholley7' OR gt_name = 'khoyt6' OR gt_name = 'whunt6' OR gt_name = 'mkersh3' OR gt_name = 'amorlan3' OR gt_name = 'jray33' OR gt_name = 'jrussell30' OR gt_name = 'csanders36' OR gt_name = 'dvaldez3' OR gt_name = 'syoon48'");
+?>
